d657e05300a3dafc4cd4924a62043b8fb466e3d6
KalanovicsZoltán <kalanovics.z@gmail.com>
2020-11-29 21:56:22 +0100

    init change

--- src/auth/AuthHandler.tsx
+++ src/auth/AuthHandler.tsx
@@ -1,17 +1,7 @@
 import axios from "axios";
 import { Token } from "typescript";
 
-/*export function setAxiosInterceptors() {
-    axios.interceptors.response.use(function (response) {
-        return response;
-    }, function (error) {
-        if (error.response.status == 403)
-            window.location.pathname = "/";
-        if (error.response.status == 401)
-            redirectToLoginPage(window.location.pathname);
-        return Promise.reject(error);
-    });
-}*/
+
 
 const setAuthHeaderToken = (token: string) => {
     if (token !== "") {
